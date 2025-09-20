eb12b3597353c5fb09c49b58d4cd88b0b8ab0d07
Atiq Rehman <your-github-email@example.com>
2025-09-20 14:03:56 +0500

    Fixed Header.css import issue

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
-import "./Header.css";
+import "./header.css";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
